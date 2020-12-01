353acb2edaf56cebe2e7eddd4ab9dbd3c2c397e7
Ahmed Maher <dba.ahmedmaher@gmail.com>
2020-12-01 10:11:24 +0200

    Update OdooInstall.sh

--- OdooInstall.sh
+++ OdooInstall.sh
@@ -39,12 +39,6 @@ INSTALL_WEBMIN="True"
 INSTALL_CERTBOT_SSL="True"
 
 
-###  WKHTMLTOPDF download links
-## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
-## in order to have correct version of wkhtmltox installed, for a danger note refer to
-## https://www.odoo.com/documentation/8.0/setup/install.html#deb ):
-WKHTMLTOX_X64=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
-WKHTMLTOX_X32=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_i386.deb
 #--------------------------------------------------
 # Update Server
 #--------------------------------------------------
@@ -73,12 +67,11 @@ sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
-echo -e "\n--- Installing Python 3 + pip3 --"
-sudo apt-get install nano git subversion bzr bzrtools python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt1-dev -y
-sudo apt-get install libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less gdebi-core -y pysassc
+echo -e "\n---- Install tool packages ----"
+sudo apt-get install wget nano subversion git bzr bzrtools python-pip python3-pip gdebi-core -y pysassc
 
 echo -e "\n---- Install python packages ----"
-sudo pip3 install -r https://github.com/odoo/odoo/raw/${OE_VERSION}/requirements.txt
+sudo apt-get install python-dateutil python-feedparser python-ldap python-libxslt1 python-lxml python-mako python-openid python-psycopg2 python-pybabel python-pychart python-pydot python-pyparsing python-reportlab python-simplejson python-tz python-vatnumber python-vobject python-webdav python-werkzeug python-xlwt python-yaml python-zsi python-docutils python-psutil python-mock python-unittest2 python-jinja2 python-pypdf python-decorator python-requests python-passlib python-pil python-gpgme python-launchpadlib build-essential python-all-dev python-setuptools python-imaging python-suds python-xlsxwriter python-wheel -y
 
 echo -e "\n---- Install python libraries ----"
 sudo pip install gdata psycogreen
@@ -102,6 +95,13 @@ sudo npm install -g rtlcss
 # Install Wkhtmltopdf if needed
 #--------------------------------------------------
 if [ $INSTALL_WKHTMLTOPDF = "True" ]; then
+  ###  WKHTMLTOPDF download links
+  ## === Ubuntu Trusty x64 & x32 === (for other distributions please replace these two links,
+  ## in order to have correct version of wkhtmltox installed, for a danger note refer to
+  ## https://www.odoo.com/documentation/8.0/setup/install.html#deb ):
+  WKHTMLTOX_X64=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+  WKHTMLTOX_X32=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_i386.deb
+  
   echo -e "\n---- Install wkhtml and place shortcuts on correct place for ODOO 9 ----"
   #pick up correct one from x64 & x32 versions:
   if [ "`getconf LONG_BIT`" == "64" ];then
@@ -117,13 +117,21 @@ else
   echo "Wkhtmltopdf isn't installed due to the choice of the user!"
 fi
 
+#--------------------------------------------------
+# Install Other Requirements
+#--------------------------------------------------
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo apt-get install -y libsasl2-dev python-dev libldap2-dev libssl-dev python3-dev
 sudo easy_install greenlet
 sudo easy_install gevent
 sudo apt-get install -y libxml2-dev libxslt1-dev zlib1g-dev python3-pip python3-wheel python3-setuptools
+sudo -H pip install -r https://raw.githubusercontent.com/odoo/odoo/10.0/requirements.txt
 sudo apt install -y python3-asn1crypto
 sudo apt install -y python3-babel python3-bs4 python3-cffi-backend python3-cryptography python3-dateutil python3-docutils python3-feedparser python3-funcsigs python3-gevent python3-greenlet python3-html2text python3-html5lib python3-jinja2 python3-lxml python3-mako python3-markupsafe python3-mock python3-ofxparse python3-openssl python3-passlib python3-pbr python3-pil python3-psutil python3-psycopg2 python3-pydot python3-pygments python3-pyparsing python3-pypdf2 python3-renderpm python3-reportlab python3-reportlab-accel python3-roman python3-serial python3-stdnum python3-suds python3-tz python3-usb python3-vatnumber python3-werkzeug python3-xlsxwriter python3-yaml
+sudo -H pip3 install -r https://raw.githubusercontent.com/odoo/odoo/11.0/requirements.txt
+sudo -H pip3 install -r https://raw.githubusercontent.com/odoo/odoo/12.0/requirements.txt
+sudo -H pip3 install -r https://raw.githubusercontent.com/odoo/odoo/13.0/requirements.txt
+sudo -H pip3 install -r https://raw.githubusercontent.com/it-projects-llc/odoo-saas-tools/12.0/requirements.txt
 sudo -H pip3 install phonenumbers
 
 #--------------------------------------------------
