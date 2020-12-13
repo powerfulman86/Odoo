80097d48ce8b8cc3cd31a4f29e766ab3286c9b58
Ahmed Maher <dba.ahmedmaher@gmail.com>
2020-12-13 09:34:20 +0200

    postgres add repository
    add repository of postgres to get latest

--- OdooInstall.sh
+++ OdooInstall.sh
@@ -60,7 +60,9 @@ sudo dpkg-reconfigure locales
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
-echo -e "\n---- Install PostgreSQL Server ----"
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
 sudo apt-get install postgresql postgresql-contrib -y
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
