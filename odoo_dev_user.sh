831a82ffc0766957c67efbfad900fd4ebe62d066
Ahmed Maher <dba.ahmedmaher@gmail.com>
2024-01-19 15:57:13 +0200

    Update odoo_dev_user.sh

--- odoo_dev_user.sh
+++ odoo_dev_user.sh
@@ -2,10 +2,12 @@
 # This script is used to add new odoo devloper user used in database engine
 #-------------------------------------------------------------------------------
 OE_USER="odoo"
+OE_group="odoo"
 
-sudo groupadd odoo
+sudo groupadd $OE_group
 
 echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
 sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
 
-sudo adduser --system --quiet --shell=/bin/bash --gecos 'ODOO' --group odoo
+sudo adduser --system --quiet --shell=/bin/bash --no-create-home --gecos 'ODOO' --group $OE_USER
+sudo adduser $OE_USER $OE_group
